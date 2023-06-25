0ca3ab43d5563dc23f101066d60d399a41dc4255
Marshal Nadar <marshalnadarmn18@gmail.com>
2023-06-25 14:02:22 +0530

    Workflow added

--- src/Component/TechDescription/TechDescription.jsx
+++ src/Component/TechDescription/TechDescription.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './TechDescription.scss';
-import TechStack from '../../Assets/Images/TechStack.webp';
+import TechStack from '../../Assets/Images/joshua-aragon-FGXqbqbGt5o-unsplash.jpg';
 import reactIcon from '../../Assets/Images/react_icon.svg';
 import jsIcon from '../../Assets/Images/js_icon.svg';
 import webpackIcon from '../../Assets/Images/webpack_icon.svg';
 import scssIcon from '../../Assets/Images/scss_icon.svg';
 import nodeJsIcon from '../../Assets/Images/nodejs_icon.svg';
 import postgresIcon from '../../Assets/Images/postgres_icon.svg';
+import reduxIcon from '../../Assets/Images/redux_icon.svg';
+import pwaIcon from '../../Assets/Images/pwa-icon.svg';
 const TechDescription = () => {
   return (
     <main>
@@ -16,6 +18,8 @@ const TechDescription = () => {
         <aside className='tech-aside-main'>
           <img src={jsIcon} className='tech-img' alt='' />
           <img src={reactIcon} className='tech-img' alt='' />
+          <img src={reduxIcon} className='tech-img' alt='' />
+          <img src={pwaIcon} className='tech-img' alt='' />
           <img src={nodeJsIcon} className='tech-img' alt='' />
           <img src={webpackIcon} className='tech-img' alt='' />
           <img src={scssIcon} className='tech-img' alt='' />
--- src/Component/WorkFlow/WorkFlow.jsx
+++ src/Component/WorkFlow/WorkFlow.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './WorkFlow.scss';
+import css from '../../Assets/Images/how-we-work_infographics-desktop_488h-1x.webp';
 
 const WorkFlow = () => {
   return (
     <main>
-      <div>WorkFlow</div>
+      <header className='workflow-header'>WorkFlow</header>
+      <div>
+        <img src={css} className='' alt='' />
+      </div>
     </main>
   );
 };

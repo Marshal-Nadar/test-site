57c6288f155848e94913da2aa0a33502f9879fa4
Marshal Nadar <marshalnadarmn18@gmail.com>
2023-07-02 17:35:56 +0530

    Fixed header animation

---
+++ src/Component/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.scss';
+const Footer = () => {
+  return (
+    <main>
+      <header className='header-text'>Footer</header>
+    </main>
+  );
+};
+
+export default Footer;
